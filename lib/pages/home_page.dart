a4fff0644e2a483e208fdad9fb6b54021223ec0e
maheshparajuli <maheshparajuli717@gmail.com>
2024-05-10 23:00:47 +0545

    new changes on homepage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,29 +1,73 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
   @override
-  State<HomePage> createState() => _HomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  List<String> todos = [];
+  TextEditingController controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: const Text("OK! LETS BUILD"),
+        backgroundColor: Colors.pink,
+        title: Text(
+          'Todo List',
+        ),
       ),
-      body:Column(
-        children: [
-          
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+              itemCount: todos.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  child: ListTile(
+                    title: Text(todos[index]),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      color: Colors.black,
+                      onPressed: () {
+                        setState(() {
+                          todos.removeAt(index);
+                        });
+                      },
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    controller: controller,
+                    decoration: InputDecoration(
+                      labelText: 'New Task',
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10.0),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      todos.add(controller.text);
+                      controller.clear();
+                    });
+                  },
+                  child: Text('Add Task'),
+                ),
+              ],
+            ),
+          ),
         ],
-      )
-        
-
-        
-      
+      ),
     );
   }
 }
